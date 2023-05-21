747711f70395c6120b4d28e5397525c06b167a19
diogomene <diogodelimamenezes15@gmail.com>
2023-05-21 12:52:51 -0400

    Iniciando header

---
+++ kdtree.h
@@ -0,0 +1,15 @@
+#ifndef KDTREE_H
+#define KDTREE_H
+
+typedef struct node{
+    void *val;
+    struct node* dir;
+    struct node* esq;
+}node;
+
+typedef struct kdtree{
+    node *raiz;
+    int (*compara)(void* a, void* b, int k);
+};
+
+#endif //KDTREE_H
