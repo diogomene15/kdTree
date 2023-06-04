d0d796fa4650830d42fcdb3c49edd192a160ecca
diogomene <diogodelimamenezes15@gmail.com>
2023-06-04 17:44:47 -0400

    Adicionando testes de objeto real

--- tests/kdtreeTests.c
+++ tests/kdtreeTests.c
@@ -3,10 +3,30 @@
 #include <stdio.h>
 #include "../kdtree.h"
 
-int comparador(const void* a, const void* b, int k){
+int comparador(const void *a, const void *b, int k) {
     //comparador de inteiros
-    int a1 = ((int*) a)[k];
-    int b1 = ((int*) b)[k];
+    int a1 = ((int *) a)[k];
+    int b1 = ((int *) b)[k];
+    return a1 - b1;
+}
+
+typedef struct latLon {
+    char dummy;
+    int lat;
+    int lon;
+} coord;
+
+int comparadorObjeto(const void *a, const void *b, int k) {
+    //comparador de inteiros
+    int a1 = 0;
+    int b1 = 0;
+    if (k == 0) {
+        a1 = ((coord *) a)->lat;
+        b1 = ((coord *) b)->lat;
+    } else {
+        a1 = ((coord *) a)->lon;
+        b1 = ((coord *) b)->lon;
+    }
     return a1 - b1;
 }
 
@@ -20,15 +40,16 @@ void testeMontarArvore() {
     assert(arvore.k == k);
     assert(arvore.comparador == comparador);
 }
-void testeInsercaoItem(){
-    kdtree* arv = (kdtree*) malloc(sizeof(kdtree));
+
+void testeInsercaoItem() {
+    kdtree *arv = (kdtree *) malloc(sizeof(kdtree));
     montarArvore(arv, 2, comparador);
 
-    int p1[] = {1,2};
-    int p2[] = {3,4};
-    int p3[] = {4,5};
+    int p1[] = {1, 2};
+    int p2[] = {3, 4};
+    int p3[] = {4, 5};
     int p4[] = {0, 0};
-    int p5[] = {0,10};
+    int p5[] = {0, 10};
 
     inserirItem(arv, p1);
     inserirItem(arv, p2);
@@ -38,42 +59,83 @@ void testeInsercaoItem(){
     inserirItem(arv, p1);
     inserirItem(arv, p5);
 
-    assert(arv->raiz->val == (void*)p1);
+    assert(arv->raiz->val == (void *) p1);
     assert(arv->raiz->pai == NULL);
 
-    assert(arv->raiz->d->val == (void*)p2);
+    assert(arv->raiz->d->val == (void *) p2);
     assert(arv->raiz->d->pai == arv->raiz);
 
-    assert(arv->raiz->d->d->val == (void*)p3);
+    assert(arv->raiz->d->d->val == (void *) p3);
     assert(arv->raiz->d->d->pai == arv->raiz->d);
 
-    assert(arv->raiz->e->val == (void*)p1);
+    assert(arv->raiz->e->val == (void *) p1);
     assert(arv->raiz->e->pai == arv->raiz);
 
-    assert(arv->raiz->e->e->val == (void*)p4);
+    assert(arv->raiz->e->e->val == (void *) p4);
     assert(arv->raiz->e->e->pai == arv->raiz->e);
 
-    assert(arv->raiz->e->e->d->val == (void*)p1);
+    assert(arv->raiz->e->e->d->val == (void *) p1);
     assert(arv->raiz->e->e->d->pai == arv->raiz->e->e);
 
-    assert(arv->raiz->e->d->val == (void*)p5);
+    assert(arv->raiz->e->d->val == (void *) p5);
+    assert(arv->raiz->e->d->pai == arv->raiz->e);
+    free(arv);
+}
+void testeInsercaoItemCoord() {
+    kdtree *arv = (kdtree *) malloc(sizeof(kdtree));
+    montarArvore(arv, 2, comparadorObjeto);
+
+    coord p1 = {'a',1, 2};
+    coord p2 = {'a',3, 4};
+    coord p3 = {'a',4, 5};
+    coord p4 = {'a',0, 0};
+    coord p5 = {'a',0, 10};
+
+    inserirItem(arv, &p1);
+    inserirItem(arv, &p2);
+    inserirItem(arv, &p3);
+    inserirItem(arv, &p1);
+    inserirItem(arv, &p4);
+    inserirItem(arv, &p1);
+    inserirItem(arv, &p5);
+
+    assert(arv->raiz->val == (void *) &p1);
+    assert(arv->raiz->pai == NULL);
+
+    assert(arv->raiz->d->val == (void *) &p2);
+    assert(arv->raiz->d->pai == arv->raiz);
+
+    assert(arv->raiz->d->d->val == (void *) &p3);
+    assert(arv->raiz->d->d->pai == arv->raiz->d);
+
+    assert(arv->raiz->e->val == (void *) &p1);
+    assert(arv->raiz->e->pai == arv->raiz);
+
+    assert(arv->raiz->e->e->val == (void *) &p4);
+    assert(arv->raiz->e->e->pai == arv->raiz->e);
+
+    assert(arv->raiz->e->e->d->val == (void *) &p1);
+    assert(arv->raiz->e->e->d->pai == arv->raiz->e->e);
+
+    assert(arv->raiz->e->d->val == (void *) &p5);
     assert(arv->raiz->e->d->pai == arv->raiz->e);
     free(arv);
 }
 
-void testeInsercaoPontos(){
-    kdtree* arv = (kdtree*) malloc(sizeof(kdtree));
+
+void testeInsercaoPontos() {
+    kdtree *arv = (kdtree *) malloc(sizeof(kdtree));
     montarArvore(arv, 2, comparador);
 
-    int p1[] = {4,5};
-    int p2[] = {1,4};
-    int p3[] = {7,4};
-    int p4[] = {-2,4};
-    int p5[] = {-4,2};
-    int p6[] = {8,6};
-    int p7[] = {9,2};
+    int p1[] = {4, 5};
+    int p2[] = {1, 4};
+    int p3[] = {7, 4};
+    int p4[] = {-2, 4};
+    int p5[] = {-4, 2};
+    int p6[] = {8, 6};
+    int p7[] = {9, 2};
 
-    int** pontos = (int**) malloc(7*sizeof(int*));
+    int **pontos = (int **) malloc(7 * sizeof(int *));
     pontos[0] = p2;
     pontos[1] = p1;
     pontos[2] = p3;
@@ -81,7 +143,7 @@ void testeInsercaoPontos(){
     pontos[4] = p5;
     pontos[5] = p6;
     pontos[6] = p7;
-    inserirPontosMedios(arv,(void**)pontos, 7, comparador);
+    inserirPontosMedios(arv, (void **) pontos, 7, comparador);
     assert(arv->raiz->val == p1);
     assert(arv->raiz->e->val == p2);
     assert(arv->raiz->e->d->val == p4);
@@ -92,27 +154,27 @@ void testeInsercaoPontos(){
     assert(arv->raiz->d->e->val == p7);
     free(pontos);
 
-    pontos = calloc(1, sizeof(int*));
+    pontos = calloc(1, sizeof(int *));
     pontos[0] = p2;
-    inserirPontosMedios(arv,(void**)pontos, 1, comparador);
+    inserirPontosMedios(arv, (void **) pontos, 1, comparador);
     assert(arv->raiz->val == p2);
     free(pontos);
     free(arv);
 }
 
-void testeAcharMaisProx(){
-    kdtree* arv = (kdtree*) malloc(sizeof(kdtree));
+void testeAcharMaisProx() {
+    kdtree *arv = (kdtree *) malloc(sizeof(kdtree));
     montarArvore(arv, 2, comparador);
 
-    int p1[] = {4,5};
-    int p2[] = {1,4};
-    int p3[] = {7,4};
-    int p4[] = {-2,4};
-    int p5[] = {-4,2};
-    int p6[] = {8,6};
-    int p7[] = {9,2};
+    int p1[] = {4, 5};
+    int p2[] = {1, 4};
+    int p3[] = {7, 4};
+    int p4[] = {-2, 4};
+    int p5[] = {-4, 2};
+    int p6[] = {8, 6};
+    int p7[] = {9, 2};
 
-    int** pontos = (int**) malloc(7*sizeof(int*));
+    int **pontos = (int **) malloc(7 * sizeof(int *));
     pontos[0] = p2;
     pontos[1] = p1;
     pontos[2] = p3;
@@ -120,31 +182,32 @@ void testeAcharMaisProx(){
     pontos[4] = p5;
     pontos[5] = p6;
     pontos[6] = p7;
-    inserirPontosMedios(arv,(void**)pontos, 7, comparador);
+    inserirPontosMedios(arv, (void **) pontos, 7, comparador);
 
-    int pontoPesquisa[] = {10,2};
-    tnode* res = acharPontoMaisProx(arv, pontoPesquisa);
+    int pontoPesquisa[] = {10, 2};
+    tnode *res = acharPontoMaisProx(arv, pontoPesquisa);
     assert(res->val == p7);
 
-    int pontoPesquisa2[] = {7,5};
+    int pontoPesquisa2[] = {7, 5};
     res = acharPontoMaisProx(arv, pontoPesquisa2);
     assert(res->val == p3);
 
-    int pontoPesquisa3[] = {4,1};
+    int pontoPesquisa3[] = {4, 1};
     res = acharPontoMaisProx(arv, pontoPesquisa3);
     assert(res->val == p1);
     free(pontos);
     free(arv);
 }
-void testaSucessor(){
-    kdtree* arv = (kdtree*) malloc(sizeof(kdtree));
+
+void testaSucessor() {
+    kdtree *arv = (kdtree *) malloc(sizeof(kdtree));
     montarArvore(arv, 2, comparador);
 
-    int p1[] = {1,2};
-    int p2[] = {3,4};
-    int p3[] = {4,5};
+    int p1[] = {1, 2};
+    int p2[] = {3, 4};
+    int p3[] = {4, 5};
     int p4[] = {0, 0};
-    int p5[] = {0,10};
+    int p5[] = {0, 10};
 
     inserirItem(arv, p1);
     inserirItem(arv, p2);
@@ -162,15 +225,16 @@ void testaSucessor(){
 
     free(arv);
 }
-void testaAntecessor(){
-    kdtree* arv = (kdtree*) malloc(sizeof(kdtree));
+
+void testaAntecessor() {
+    kdtree *arv = (kdtree *) malloc(sizeof(kdtree));
     montarArvore(arv, 2, comparador);
 
-    int p1[] = {1,2};
-    int p2[] = {3,4};
-    int p3[] = {4,5};
+    int p1[] = {1, 2};
+    int p2[] = {3, 4};
+    int p3[] = {4, 5};
     int p4[] = {0, 0};
-    int p5[] = {0,10};
+    int p5[] = {0, 10};
 
     inserirItem(arv, p1);
     inserirItem(arv, p2);
@@ -188,9 +252,10 @@ void testaAntecessor(){
     free(arv);
 }
 
-int main(){
+int main() {
     testeMontarArvore();
     testeInsercaoItem();
+    testeInsercaoItemCoord();
     testeInsercaoPontos();
     testeAcharMaisProx();
     testaSucessor();
